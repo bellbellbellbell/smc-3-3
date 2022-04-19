b20be08c25a98a35657eb7fd0faabd183f94e1bf
bellbellbellbell <smc2010107@sonline20.sen.go.kr>
2022-04-19 14:50:58 +0900

    0419 프로젝트 수정(scan.java 추가

--- 0419/src/scan.java
+++ 0419/src/scan.java
@@ -1,9 +1,19 @@
-
+import java.util.Scanner;
 public class scan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		System.out.print("1000이하의 자연수 입력");
+		int a = scan.nextInt();
+		int sum = 0;
+		for(int i=1; i<=a; i++) {
+			if(i%4==0) {
+				System.out.print(i);
+				sum+=i;
+			}
+		}
+		System.out.println("4의 배수 합: " + sum + "입니다.");
 	}
 
 }
