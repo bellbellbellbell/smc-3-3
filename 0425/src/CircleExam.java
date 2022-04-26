ba7014184c14b7fd29c4350af86010ea153a5ce0
bellbellbellbell <smc2010107@sonline20.sen.go.kr>
2022-04-26 09:46:04 +0900

    0425 프로젝트 추가

---
+++ 0425/src/CircleExam.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CircleExam {
+	public static void main(String args[]) {
+		 Scanner scan = new Scanner(System.in);
+		 System.out.print("반지름 값 >>");
+		 int radius = scan.nextInt
+		 System.out.println("������ "+ radius + "�� ���� ���̴� " + c.Area() + "�̴�.");  // ��¹�
+	}
+}
+
+class Circle{
+	int radius;
+	 // ������(radius) �� �ʱ�ȭ
+	
+	public double Area() {   
+		// ���� ���� return (���� ���� = ������*������*3.14)
+	}
+}
